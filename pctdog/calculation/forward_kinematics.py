a2acc458124dbd7213f7bdfc28a594c116412bcc
pct-hbriji <riji@qq.com>
2020-07-27 15:23:20 +0800

    整理目录

--- pctdog/calculation/forward_kinematics.py
+++
@@ -1,8 +0,0 @@
-import pctdog
-import time
-def positive_solution():
-    global cac_CSYS
-    cac_CSYS = int_CSYS
-    execute()
-    time.sleep(1)
-    #pctdog.height(100)
--- pctdog/gait/IMU.py
+++
@@ -1,7 +0,0 @@
-import pyb
-def imu():
-	acc = pyb.Accel()
-	x = acc.y()
-	y = acc.x()
-	z = acc.z()
-	print("x:",x*90/25," y:",y*90/25," z:",z*90/25)
